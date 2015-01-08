75156eb5bd22850d1902f474a9495967ea4666f8
Stan <malkin.stanislav@gmail.com>
2015-01-08 23:24:02 +0200

    added basic examples for installing latest ruby, php, nodejs with provisioning

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y python-software-properties
+apt-get install -y python-software-properties curl
+
+# you can get latest php like this
 add-apt-repository ppa:ondrej/php5
 
-apt-get -q -y install php5 php5-cli ruby nginx
+# or nodejs like this
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+
+# or rvm/ruby like this
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -L https://get.rvm.io | bash -s stable --ruby
+
+# now lets install it along with nginx
+apt-get -q -y install nodejs php5 php5-cli php5-fpm nginx
