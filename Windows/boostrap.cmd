2df0ceb15dc57f1926f9bec7d188b2491bcd3eeb
Stan <malkin.stanislav@gmail.com>
2015-01-27 19:28:53 +0200

    Added Chocolatey for provisioning

---
+++ Windows/boostrap.cmd
@@ -0,0 +1,5 @@
+echo 'Installing Chocolatey..'
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+echo 'Installing SQL Server 2014 Express (UNATTENDED)..."
+choco install mssqlserver2014express
